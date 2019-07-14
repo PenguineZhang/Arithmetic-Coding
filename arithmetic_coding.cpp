358b3b5dc3a0f2e3ebd7b15bc066ebe21efc991c
Peng Zhang <pengzhang40@gmail.com>
2019-07-14 12:22:52 -0700

    partially finish encoding

--- arithmetic_coding.cpp
+++ arithmetic_coding.cpp
@@ -1,4 +1,5 @@
 #include <arithmetic_coding.hpp>
+#include <algorithm>
 #include <cassert>
 
 arithmetic_coding::arithmetic_coding(std::string text, std::map<char, float> symbols_prob):_symbols(text){
@@ -20,20 +21,36 @@ void arithmetic_coding::print_info(){
 	for (auto& i : _symbols_prob){
 		std::cout << "\033[1;31m" << i.first << ": " << "\033[0m";
 		for (auto& j: _symbols_prob[i.first]){
-			std::cout << "\033[1;32m" << j << " " ;
+			std::cout << "\033[1;32m" << j << "\t\t" ;
 		}
-		std::cout << "\033[0m" << std::endl;
+		std::cout << std::endl;
 	}
+	std::cout << "Encoded interval: (" << _encoded_interval.first << ", " << _encoded_interval.second << ")\033[0m" << std::endl;
 }
 
 void arithmetic_coding::encode(){
-	float interval_lower_bound = 0.0;
-	float interval_upper_bound = 0.0;
+	float symbols_lower, symbols_upper, selected_interval, new_start;
 	for ( auto& letter: _symbols ){
 
-		int selected_interval = _symbols_prob[ letter ][2] - _symbols_prob[ letter ][1];
+		symbols_lower = _symbols_prob[ letter ][1];
+		symbols_upper = _symbols_prob[ letter ][2];
+		selected_interval = symbols_upper - symbols_lower;
 
+		new_start = symbols_lower;
+
+		for ( auto& i: _symbols_prob ){
+			i.second[1] = new_start;
+			i.second[2] = i.second[0] * selected_interval + i.second[1];
+			new_start = i.second[2];
+		}
 	}
+	std::map<char, std::vector<float>>::iterator it_begin = _symbols_prob.begin();
+	std::map<char, std::vector<float>>::iterator it_end = _symbols_prob.rbegin();
+	std::cout << it_begin->first << std::endl;
+	std::cout << it_end->first << std::endl;
+	_encoded_interval.first = _symbols_prob[it_begin->first][1];
+	_encoded_interval.second = _symbols_prob[it_end->first][2];
+
 }
 
 void arithmetic_coding::decode(){
--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@
 int main(int argc, char** argv){
 
 	std::map<char, float> symbol_prob;
-	symbol_prob.insert(std::pair<char, float>('a', 0.8));
-	symbol_prob.insert(std::pair<char, float>('b', 0.2));
+	symbol_prob.insert(std::pair<char, float>('a', 0.4));
+	symbol_prob.insert(std::pair<char, float>('b', 0.4));
+	symbol_prob.insert(std::pair<char, float>('c', 0.2));
 
-	std::string text("aaba");
+	std::string text("aabacc");
 
 	arithmetic_coding ac(text, symbol_prob);
-	ac.print_info();
+	// ac.print_info();
 	ac.encode();
+	ac.print_info();
 
 
 	return 0;
